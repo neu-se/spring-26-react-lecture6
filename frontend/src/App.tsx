9e1afef5cdeaaf8862ac69967027be6e3bdc7d38
Rob Simmons <robsimmons@gmail.com>
2026-01-28 14:29:59 -0500

    Section 6 end point for lecture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { z } from "zod";
 import OffsetClock from "./OffsetClock.tsx";
@@ -7,13 +7,22 @@ import OffsetClock from "./OffsetClock.tsx";
 const CLOCK_URI = "https://clock-socket.onrender.com";
 
 /** Expected payload for the "/api/status" GET endpoint */
-const zStatusPayload = z.object({ currentTick: z.iso.datetime(), currentCount: z.int().gte(0) });
+const zStatusPayload = z.object({
+  currentTick: z.iso.datetime(),
+  currentCount: z.int().gte(0),
+});
 
 /** Expected payload for the "count" socket.io event */
-const zCountPayload = z.object({ count: z.int().gte(0), watchers: z.int().gte(0) });
+const zCountPayload = z.object({
+  count: z.int().gte(0),
+  watchers: z.int().gte(0),
+});
 
 /** Expected payload for the "tick" socket.io event */
-const zTickPayload = z.object({ time: z.iso.datetime(), watchers: z.int().gte(0) });
+const zTickPayload = z.object({
+  time: z.iso.datetime(),
+  watchers: z.int().gte(0),
+});
 
 const CLOCKS = [
   { id: "Atlanta", offset: -5 },
@@ -23,16 +32,63 @@ const CLOCKS = [
   { id: "Tokyo", offset: 9 },
 ];
 
+function useSocketConnection(setError: (error: string) => void, isWatching: boolean) {
+  const [now, setNow] = useState<null | Date>(null);
+
+  useEffect(() => {
+    fetch(CLOCK_URI + "/api/status")
+      .then((response) => response.json())
+      .then((json) => {
+        const data = zStatusPayload.parse(json);
+        setNow(new Date(data.currentTick));
+      })
+      .catch((err) => setError(`Unexpected error when fetching: ${err}`));
+  }, []);
+
+  const [watchers, setWatchers] = useState<null | number>(null);
+  useEffect(() => {
+    if (!isWatching) {
+      console.log(`Not watching, no web socket connection`);
+      return () => {
+        console.log(`Not watching, so no cleanup to do`);
+      };
+    }
+    console.log(`Establishing web socket connection`);
+    const socket = io(CLOCK_URI);
+    socket.on("tick", (payload) => {
+      const data = zTickPayload.parse(payload);
+      setNow(new Date(data.time));
+      setWatchers(data.watchers);
+    });
+    return () => {
+      console.log(`Closing web socket connection`);
+      socket.disconnect();
+    };
+  }, [isWatching]);
+
+  return { now, watchers };
+}
+
 export default function App() {
   const [error, setError] = useState<null | string>(null);
-  const [now, setNow] = useState<Date>(new Date());
+  const [isWatching, setIsWatching] = useState(false);
+  const { now, watchers } = useSocketConnection(setError, isWatching);
 
   return (
     <div style={{ width: 600, margin: "auto" }}>
-      <div style={{ display: "flex", flexDirection: "row", gap: "1em" }}>
-        {CLOCKS.map(({ id, offset }) => (
-          <OffsetClock key={id} city={id} offset={offset} datetime={now} />
-        ))}
+      {watchers !== null && `There are ${watchers} watcher(s)`}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1em",
+        }}
+      >
+        {now === null && "Loading..."}
+        {now !== null &&
+          CLOCKS.map(({ id, offset }) => (
+            <OffsetClock key={id} city={id} offset={offset} datetime={now} />
+          ))}
       </div>
       <div
         style={{
@@ -42,14 +98,16 @@ export default function App() {
           paddingBlock: "1em",
         }}
       >
-        <button onClick={() => setNow(new Date())}>Update time</button>
-        {error === null ? (
+        {error === null && (
           <button onClick={() => setError("Why did you create problems on purpose?")}>
             Create an error
           </button>
-        ) : (
-          <button onClick={() => setError(null)}>Clear errors</button>
         )}
+        {error !== null && <button onClick={() => setError(null)}>Clear errors</button>}
+        <button onClick={() => setIsWatching(!isWatching)}>
+          {isWatching && "Stop"}
+          {!isWatching && "Start"} watching
+        </button>
       </div>
       {error && <div style={{ color: "red" }}>{error}</div>}
     </div>
