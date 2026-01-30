aedd7b739c6437be2c09ef93450c0187245f9b0b
Rob Simmons <robsimmons@gmail.com>
2026-01-30 11:25:40 -0500

    tmp, change column width

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,22 @@ import OffsetClock from "./OffsetClock.tsx";
 const CLOCK_URI = "https://clock-socket.onrender.com";
 
 /** Expected payload for the "/api/status" GET endpoint */
-const zStatusPayload = z.object({ currentTick: z.iso.datetime(), currentCount: z.int().gte(0) });
+const zStatusPayload = z.object({
+  currentTick: z.iso.datetime(),
+  currentCount: z.int().gte(0),
+});
 
 /** Expected payload for the "count" socket.io event */
-const zCountPayload = z.object({ count: z.int().gte(0), watchers: z.int().gte(0) });
+const zCountPayload = z.object({
+  count: z.int().gte(0),
+  watchers: z.int().gte(0),
+});
 
 /** Expected payload for the "tick" socket.io event */
-const zTickPayload = z.object({ time: z.iso.datetime(), watchers: z.int().gte(0) });
+const zTickPayload = z.object({
+  time: z.iso.datetime(),
+  watchers: z.int().gte(0),
+});
 
 const CLOCKS = [
   { id: "Atlanta", offset: -5 },
@@ -36,15 +45,28 @@ export default function App() {
         const data = zStatusPayload.parse(json);
         setNow(new Date(data.currentTick));
       })
-      .catch((err) => setError(`Unexpected error when fetching: ${err}`));
+      .catch((err) =>
+        setError(`Unexpected error when fetching: ${err}`),
+      );
   }, []);
 
   return (
     <div style={{ width: 600, margin: "auto" }}>
-      <div style={{ display: "flex", flexDirection: "row", gap: "1em" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1em",
+        }}
+      >
         {now &&
           CLOCKS.map(({ id, offset }) => (
-            <OffsetClock key={id} city={id} offset={offset} datetime={now} />
+            <OffsetClock
+              key={id}
+              city={id}
+              offset={offset}
+              datetime={now}
+            />
           ))}
       </div>
       <div
@@ -56,11 +78,19 @@ export default function App() {
         }}
       >
         {error === null ? (
-          <button onClick={() => setError("Why did you create problems on purpose?")}>
+          <button
+            onClick={() =>
+              setError(
+                "Why did you create problems on purpose?",
+              )
+            }
+          >
             Create an error
           </button>
         ) : (
-          <button onClick={() => setError(null)}>Clear errors</button>
+          <button onClick={() => setError(null)}>
+            Clear errors
+          </button>
         )}
       </div>
       {error && <div style={{ color: "red" }}>{error}</div>}
